2d80d77f4dd47f0b2bf426da691d3daa1ca4354f
work <work@bes-bidmax-006.(none)>
2016-09-26 16:56:25 +0800

    modified online

--- lua_srv/conf/lua/subrequest.lua
+++ lua_srv/conf/lua/subrequest.lua
@@ -7,7 +7,16 @@ local data = ngx.req.get_body_data();
 -- get request uri's args
 local args = ngx.req.get_uri_args();
 
-res = ngx.location.capture("/sub2"..ngx.var.request_uri);
+-- issue subrequest.
+local res1,res2,res3 = ngx.location.capture_multi({
+    {"/sub1"..ngx.var.request_uri},
+    --{"/sub2"..ngx.var.request_uri},
+    --{"/sub3"..ngx.var.request_uri},
+    --{"/sub4"..ngx.var.request_uri},
+    --{"/sub5"..ngx.var.request_uri},
+    --{"/sub6"..ngx.var.request_uri},
+    --{"/sub7"..ngx.var.request_uri}
+    -- TODO: N's subrequests
+})
 
-ngx.say(res.body);
-
+ngx.say("");
